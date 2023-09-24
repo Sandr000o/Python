11fa8e767fecabf751e983e791eb08ca806755be
Sandr000o <lisandro4842@gmail.com>
2023-09-24 01:03:29 -0500

    leer diccionarios

---
+++ bucles/iterar_listas/mas_iteraciones.py
@@ -0,0 +1,25 @@
+
+frutas=["banana","manzana","ciruela","pera","naranja","granada","durazno"]
+cadena="Hola"
+numeros=[2,5,8,10]
+
+#Evitando que se coma una banana con la sentencia continue
+for fruta in frutas:
+    if(fruta=='banana'):
+        continue 
+    print(f"Me voy a comer una {fruta}")
+    
+#Evitar que el bucle siga ejecutandose, el else no se ejecuta tampoco después del break
+for fruta in frutas:
+    if(fruta=='naranja'):
+        break 
+    print(f"Me voy a comer una {fruta}")
+    print("Ya no puedes seguir comiendo porque le hizo daño la naranja")
+    
+#Recorrer una cadena de texto
+for letra in cadena:
+    print(letra)
+    
+#For en una sola línea de código
+
+for 
